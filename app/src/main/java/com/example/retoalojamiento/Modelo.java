89447a87f547c9dbd14db67401c167133a7188de
IN1DM3B_01 <christian.vonheerenri@elorrieta-errekamari.com>
2020-01-21 09:06:12 +0100

    trabajando en la seleccion de alojamientos

---
+++ app/src/main/java/com/example/retoalojamiento/Modelo.java
@@ -0,0 +1,13 @@
+package com.example.retoalojamiento;
+
+import java.util.ArrayList;
+
+public class Modelo {
+
+    ArrayList<Alojamiento> alojamientos;
+
+    public Modelo() {
+        alojamientos = new ArrayList<Alojamiento>();
+    }
+
+}
